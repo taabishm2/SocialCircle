862d1f02877ea6258c8264d7a4a34f26893c9711
Victor Suciu <victorsuciu@Victors-MacBook-Pro.local>
2023-03-04 13:44:13 -0600

    added initial pytorch model

---
+++ matching_model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class MatchingModel(nn.Module):
+
+    def __init__(self, num_attributes):
+        super().__init__()
+
+        self.layers = nn.Sequential([
+            nn.Linear(num_attributes * 2, num_attributes // 4),
+            nn.ReLU(),
+            nn.Linear(num_attributes // 4, 1),
+            nn.Sigmoid()
+        ])
+
+    def forward(self, x):
+        return self.layers(x)
+        

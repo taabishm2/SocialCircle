b7a11cb308f13ec89a98b5ca90bfe8f0f25c261a
Soldier4893 <58051347+Soldier4893@users.noreply.github.com>
2023-03-04 16:41:11 -0600

    fixed test data

--- user.py
+++ user.py
@@ -4,10 +4,17 @@ import random
 
 def create_csv(num_user, num_att, num_per, num_tag, max_att, max_err):
     data = np.zeros((num_user, 1 + num_tag + num_att))
+    
+    # add personalities
     data.T[0] = np.random.randint(low = 0, high = num_per, size=num_user)
+    
+    # personality vectors
     prefs = {i: np.random.RandomState(i).uniform(-max_att,max_att, num_att) for i in range(num_per)}
     for col in data:
-        col[1:num_tag+1] = np.random.randint(low=0, high=1+num_att, size=num_tag)
+        # set tags
+        col[1:num_tag+1] = np.random.choice(num_att, 0, replace=False)
+        
+        # set preferences
         error = np.random.uniform(-max_err,max_err, num_att)
         col[num_tag+1:num_tag+num_att+1] = np.around(error + prefs[int(col[0])], 3)
     df = pd.DataFrame(data)

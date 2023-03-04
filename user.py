5fe6c72c425a038651e29069b6b3dff6cf3500c8
Soldier4893 <58051347+Soldier4893@users.noreply.github.com>
2023-03-04 17:01:23 -0600

    fixed labelling

--- user.py
+++ user.py
@@ -12,10 +12,20 @@ def create_csv(num_user, num_att, num_per, num_tag, max_att, max_err):
     prefs = {i: np.random.RandomState(i).uniform(-max_att,max_att, num_att) for i in range(num_per)}
     for col in data:
         # set tags
-        col[1:num_tag+1] = np.random.choice(num_att, 0, replace=False)
+        col[1:num_tag+1] = np.random.choice(num_att, num_tag, replace=False,)
         
         # set preferences
         error = np.random.uniform(-max_err,max_err, num_att)
         col[num_tag+1:num_tag+num_att+1] = np.around(error + prefs[int(col[0])], 3)
+    
+    # export csv
     df = pd.DataFrame(data)
-    df.to_csv('users.csv')
+    df.reset_index(drop=True, inplace=True)
+    pers = ["Personality"]
+    tags = [f"T_{i}" for i in range(num_tag)]
+    prefs = [f"A_{i}" for i in range(num_att)]
+    cols = pers + tags + prefs
+    df.columns = cols
+    df.to_csv('users.csv', index = False)
+    
+create_csv(100, 20, 16, 5, 1, .1)

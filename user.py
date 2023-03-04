1f7575897d9b06c66e6dec61ed1d8f159821eb89
Soldier4893 <58051347+Soldier4893@users.noreply.github.com>
2023-03-04 15:14:31 -0600

    random tree created

--- user.py
+++ user.py
@@ -1,27 +1,33 @@
 import numpy as np
 import pandas as pd
-from binarytree import build
+from binarytree import tree
 import random
 
 
-random.seed(1)
+random.seed(9)
 class user():
     def __init__(self, P, attributes):
-        self.tree = tree(P)
+        self.tree = decisionTree(P, 4)
         self.attributes = attributes
     
-class tree():
-    def __init__(self, P):
+class decisionTree():
+    def __init__(self, P, h):
         self.P = P
-        self.root = self.createTree(P, 10)
+        self.h = h
+        self.root = self.createTree(P, h)
         
     
-    def createTree(self, P, size):
-        values = random.sample(range(size), size)
-        root = build(values)
+    def createTree(self, P, h):
+        root = tree(height=h, is_perfect=False)
         print(root)
         return root
     
     def attributeScore(self, attributes):
-        pass
+        curr = self.tree
+        for i in range(self.h):
+            attribute = attributes.index(curr)
+            
+        print(attribute)
+    
 
+sn = user("A", "B")

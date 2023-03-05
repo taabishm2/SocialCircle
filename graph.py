9640e4fec6c9a0ddde2958be42c1ce1e640af4b4
Soldier4893 <58051347+Soldier4893@users.noreply.github.com>
2023-03-05 00:58:19 -0600

    remake graph file

--- graph.py
+++ graph.py
@@ -5,19 +5,16 @@ import numpy as np
 import time
 
 
-# =============================================================================
 # num_nodes = 600
-# s = time.time()
-# edges = np.random.choice([0, 1], size=(num_nodes, num_nodes))
 # edge_to_weight = {(i, j) : np.random.rand() for i in range(num_nodes) for j in range(num_nodes) 
 #                   if np.random.rand() < 0.05 and i != j}
 # 
-# =============================================================================
-def plot_graph(edges, edge_to_weight, num_nodes):
+
+def plot_graph(edge_to_weight, num_nodes):
     rng = np.random.RandomState(0)
     pos = {i:(rng.rand(), rng.rand()) for i in range(num_nodes)}
     G = nx.Graph()
     G.add_weighted_edges_from([(i, j, w) for (i, j), w in edge_to_weight.items()])
     edges,weights = zip(*nx.get_edge_attributes(G,'weight').items())
-    nx.draw(G, node_size = 10, node_color = '#000000', width = .1, edge_color = weights, edge_cmap=plt.cm.Blues, pos = pos)
+    nx.draw(G, node_size = 5, node_color = '#000000', width = .1, edge_color = weights, edge_cmap=plt.cm.Blues, pos = pos)
 
